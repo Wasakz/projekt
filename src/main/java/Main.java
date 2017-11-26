ba3edc5a1de0c5ee6f4b18804fa73c727d27c9ab
Piotr <wasakmail@gmail.com>
2017-11-26 12:54:43 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        System.out.println();
+       Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/workdb", "SA", "");
+    }
+}
